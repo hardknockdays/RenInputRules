6961ebf34ed1e87f6a703232f4e791b372ff7a9c
RENPWN <46776762+hardknockdays@users.noreply.github.com>
2025-09-25 00:16:03 +0700

    Update RenInputRules.js

--- RenInputRules.js
+++ RenInputRules.js
@@ -1,62 +1,61 @@
-	(function ( $ ) {
- 
-		$.fn.RenInputRules = function( options ) {
-	 
-			// This is the easiest way to have default options.
-			var set = $.extend({
-				// These are the defaults.
-				patnnumstat		: false,
-				patntextstat	: false,
-				
-				patnnum 		: '[0-9.]',
-				patntext		: '[a-zA-Z0-9!/.-\s_].{0,}',
-				
-				regexnum		: /^[0-9]+\.[0-9][0-9][0-9]/gi, //for number
-				regexdef		: /[^a-zA-Z0-9().|\/!-\s_]/gi, // for all input
-				regexcustoms	: /[^a-zA-Z0-9.]/gi, // for customs class
-				
-				
-			}, options );
-			
-			return this.each(function() {
-				// Plugin code would go here...
-				var e = $(this);
-				
-				if(typeof e.attr('disabled') === 'undefined' && typeof e.attr('readonly') === 'undefined'){
-					if(set.patnnumstat)
-						$('input[type="number"]').prop('pattern', set.patnnum);
-					
-					if(set.patntextstat)
-						$('input[type="text"]').prop('pattern', set.patntext);
-				}
-				
-				// e.css({
-					// color: settings.color,
-					// backgroundColor: settings.backgroundColor
-				// });				
-				
-				// $('body').delegate('input', "keypress keyup", function(event){
-				e.on("keypress keyup", function(event){
-					// var e = $(this);
-					
-					if(typeof e.attr('disabled') === 'undefined' && typeof e.attr('readonly') === 'undefined'){
-						if(e.prop('type') === 'number')
-							e.val(e.val().replace(set.regexnum, ""));
-						else if($(event.target).parent().attr('class') == 'easy-autocomplete')
-							e.val(e.val().replace(set.regexcustoms, ""));
-						else
-							e.val(e.val().replace(set.regexdef, ""));
-						
-						if(e.prop('type') === 'number' && event.type === 'keypress'){
-							if ((event.which < 48 || event.which > 57)){
-								event.preventDefault();
-							}else if(e.val()[0] === '0'){
-								e.val("");
-							}
-						}
-					}
-				});
-			});
-		};
-	 
-	}( jQuery ));
+(function ($) {
+  $.fn.RenInputRules = function (options) {
+    // Default options
+    var set = $.extend({
+      patnnumstat: false,
+      patntextstat: false,
+
+      patnnum: '^[0-9]*\\.?[0-9]*$', // lebih fleksibel: angka + optional titik
+      patntext: '^[a-zA-Z0-9!/\\.\\-\\s_]+$',
+
+      regexnum: /[^0-9.]/g,                // hanya angka & titik
+      regexdef: /[^a-zA-Z0-9().|\\/!._\\-\\s]/g, // default allowed
+      regexcustoms: /[^a-zA-Z0-9.]/g       // untuk input easy-autocomplete
+    }, options);
+
+    function cleanValue($el, event) {
+      if ($el.is('[disabled], [readonly]')) return;
+
+      let val = $el.val();
+
+      if ($el.prop('type') === 'number') {
+        val = val.replace(set.regexnum, "");
+      } else if ($el.closest('.easy-autocomplete').length) {
+        val = val.replace(set.regexcustoms, "");
+      } else {
+        val = val.replace(set.regexdef, "");
+      }
+
+      // blok leading zero hanya untuk angka integer, bukan decimal
+      if ($el.prop('type') === 'number' && /^0[0-9]+$/.test(val)) {
+        val = val.replace(/^0+/, '');
+      }
+
+      $el.val(val);
+
+      // prevent input non-digit di keypress untuk number
+      if ($el.prop('type') === 'number' && event.type === 'keypress') {
+        if (event.which < 48 || event.which > 57) {
+          event.preventDefault();
+        }
+      }
+    }
+
+    return this.each(function () {
+      var $el = $(this);
+
+      if (!$el.is('[disabled], [readonly]')) {
+        if (set.patnnumstat && $el.prop('type') === 'number') {
+          $el.prop('pattern', set.patnnum);
+        }
+        if (set.patntextstat && $el.prop('type') === 'text') {
+          $el.prop('pattern', set.patntext);
+        }
+      }
+
+      $el.on("input keypress keyup", function (event) {
+        cleanValue($el, event);
+      });
+    });
+  };
+}(jQuery));
